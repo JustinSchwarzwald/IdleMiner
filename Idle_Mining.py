3530cb1883ec0067ad8fe56cccf1c68c71cd0f7a
Justin Schwarzwald <JustinSchwarzwald@gmail.com>
2021-08-31 22:03:44 -0700

    Added MSI Afterburner
    Must Run program in terminal with admin privileges.

--- Idle_Mining.py
+++ Idle_Mining.py
@@ -6,18 +6,20 @@ import os
 
 # GI.help()
 avg = np.zeros((50,), dtype=float)
+avg = np.append(avg, 1000000.0)
 
 
 while(1):
     avg = np.delete(avg,0)
     new_Value = GI.getGPUIndex(2)
     avg = np.append(avg,new_Value)
-    time.sleep(1)
     print(np.mean(avg))
+    time.sleep(30)
 
-    if(np.mean(avg) < 20.0):
+    if(np.mean(avg) < 60.0):
+
         os.chdir("C:\\Program Files (x86)\\MSI Afterburner")
-        sp.Popen("C:\\Program Files (x86)\\MSI Afterburner\\MSIAfterburner.exe -profile2")
+        sp.run("C:\\Program Files (x86)\\MSI Afterburner\\MSIAfterburner.exe" + " -Profile2")
         os.chdir('C:\\Users\\Justin\\Downloads\\Mining\\NBMiner_39.1_Win\\NBMiner_Win')
         sp.Popen('C:\\Users\\Justin\\Downloads\\Mining\\NBMiner_39.1_Win\\NBMiner_Win\\start_eth.bat')
         while(1):
